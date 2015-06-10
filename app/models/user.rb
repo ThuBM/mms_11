ee5a5ab6af6a135ae7a471beee9da506ec8be489
Nguyen Van Duc <duc.nv226@gmail.com>
2015-06-10 14:40:41 +0700

    client login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
   has_many :user_positions
   has_many :positions, through: :user_positions
   has_many :user_teams
