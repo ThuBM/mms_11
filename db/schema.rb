1d677d077d6b7d20380241624545f00628a69381
Nguyen Van Duc <duc.nv226@gmail.com>
2015-06-10 14:40:41 +0700

    client login

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150610013447) do
+ActiveRecord::Schema.define(version: 20150610075447) do
 
   create_table "activities", force: :cascade do |t|
     t.datetime "time"
@@ -99,13 +99,26 @@ ActiveRecord::Schema.define(version: 20150610013447) do
   add_index "user_teams", ["user_id"], name: "index_user_teams_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "name",       limit: 255
+    t.string   "name",                   limit: 255
     t.datetime "birthday"
-    t.string   "action",     limit: 255
-    t.string   "role",       limit: 255
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.string   "action",                 limit: 255
+    t.string   "role",                   limit: 255
+    t.datetime "created_at",                                      null: false
+    t.datetime "updated_at",                                      null: false
+    t.string   "email",                  limit: 255, default: "", null: false
+    t.string   "encrypted_password",     limit: 255, default: "", null: false
+    t.string   "reset_password_token",   limit: 255
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          limit: 4,   default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip",     limit: 255
+    t.string   "last_sign_in_ip",        limit: 255
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   add_foreign_key "project_users", "projects"
   add_foreign_key "project_users", "users"
