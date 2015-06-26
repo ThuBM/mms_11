f4f1bacd51203eba2546908f2d86fd435e2f5335
Bui Minh Thu <buiminhthuk55@gmail.com>
2015-06-26 11:02:27 +0700

    add not complete

--- app/controllers/admin/projects_controller.rb
+++ app/controllers/admin/projects_controller.rb
@@ -1,6 +1,8 @@
 class Admin::ProjectsController < ApplicationController
   def new
     @project = Project.new
+    @teams = Team.all
+    #@members = Team.find(params[:team_id]).members unless params[:id].nil?
     @members = User.all
   end
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,6 @@
 class Project < ActiveRecord::Base
   has_many :project_users
+  has_many :members, class_name: "User", through: :project_users, source: :user
   belongs_to :team
   belongs_to :leader, class_name: "User", inverse_of: :project
 end
